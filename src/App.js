0ecebbf8152690220d62cd2de5b729edde57de59
Josiah White <josiahwhite@MacBook-Pro-3.local>
2021-02-26 14:11:26 +1030

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="appWrapper">
+      <div />
+      <div className="content">
+        <p>awda</p>
+      </div>
+      <div />
+    </div>
+  );
+}
+
+export default App;
